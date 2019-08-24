344e395b2d2c7ec5948707748b341f9325e4ef00
ChiAlphaTau <Committer>
2019-08-24 15:54:20 +0100

    Mainly enabled physics.

--- js/entities/player.js
+++ js/entities/player.js
@@ -2,6 +2,8 @@ export class Player {
     constructor (scene, color, health, x, y) {
         this.color = color;
         this.health = health;
-        this.sprite = scene.add.sprite(x, y, 'player');
+        this.sprite = scene.physics.add.sprite(x, y, 'player');
+        this.sprite.setBounce(0.2);
+        this.sprite.setCollideWorldBounds(true);
     }
 }
--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ let config = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
+    physics: {
+        default: "arcade",
+            arcade: {
+                gravity: {y: 300},
+                debug: false
+            }
+    },
 	scene: [LoadScene, GameScene]
 }
 
