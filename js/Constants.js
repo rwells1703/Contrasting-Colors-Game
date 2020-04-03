5269fd74de21e75b74770dee3f2c8517bdc0b6a4
Richard Wells <rwells1703@gmail.com>
2020-04-03 22:46:11 +0100

    Minor changes to physics constants for better movement.

--- js/Constants.js
+++ js/Constants.js
@@ -25,22 +25,21 @@ export const TEXTURE_SIZE = 64;
 
 export const DRAGCOEFF = 0.8;
 export const FRICTCOEFF = 0.3;
-export const LOCOMOTIVE = 500;
+export const LOCOMOTIVE = 450;
 export const AIRCOEFF = 0.2;
 
 export const XDROPOFF = 0.1;
 
 export const GFS = 1500;
-export const JUMPVEOLCITY = -700;
+export const JUMPVEOLCITY = -650;
 
 export const PBLOBLAUNCH = 1000;
-export const BLOBBOUNCECOEFF = 0.7;
+export const BLOBBOUNCECOEFF = 0.5;
 export const BLOBTIMEOUT = 500;
 export const MAXBLOBBOUNCES = 3;
 
 export const MAX_PLAYER_HEALTH = 1;
 export const MAX_ENEMY_HEALTH = 1;
-export const MAX_BOSS_HEALTH = 15;
 
 export const TITLE_FONT = {font: "50px Impact"};
 export const BUTTON_FONT = {font: "30px Impact"};
